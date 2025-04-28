6c7366feb6eda66e0547f9e703c0ce019f70927e
Honey Deshmukh <8697honeydeshmukh@gmail.com>
2025-04-28 14:41:09 +0530

    Add files via upload

---
+++ functions/function_practical/main.js
@@ -0,0 +1,47 @@
+
+// wap to create a function to generate table of user given number.
+// wap to print a 1 to n table using a function.
+// wap to print a 1 to 5 table using a function as explain in class.
+
+// 1. 
+
+// let table_number = parseInt(prompt("Enter a number"));
+
+// function table(x) {
+//     for (let i = 1; i <= 10; i++) {
+//         document.writeln(x + " * " + i + " = " + (x * i) + "<br>");
+//     }
+// }
+
+// table(table_number);
+
+
+// // 2. 
+
+// let number = parseInt(prompt("Enter a number"));
+// function tables(){
+//     for(let i=1; i<=number; i++){
+//         for(let j=1; j<=10; j++){
+//        document.writeln( i + "*" + j + "=" + (i*j)+ "<br>")
+//     }
+//     document.writeln("<br>")
+// }
+// }
+// tables();
+
+// // 3. 
+
+// function printTables() {
+//     for (let i = 1; i <= 10; i++) {
+//      for (let j = 1; j <= 5; j++) {
+//             document.writeln(j + " x " + i + " = " + (j * i) + "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp");
+//         }
+//         document.writeln("<br>");
+//     }
+// }
+
+// printTables();
+
+
+
+
